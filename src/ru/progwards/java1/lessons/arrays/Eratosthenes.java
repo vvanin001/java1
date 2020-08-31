8422a615ba5044910d13656020288f19ffad1ff5
home <86@mail.ru>
2020-08-31 19:59:54 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/ru/progwards/java1/lessons/classes/Animal.java
    #	src/ru/progwards/java1/lessons/classes/ComplexNum.java
    #	src/ru/progwards/java1/lessons/classes/Cow.java
    #	src/ru/progwards/java1/lessons/classes/Duck.java
    #	src/ru/progwards/java1/lessons/classes/Hamster.java

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -3,6 +3,17 @@ package ru.progwards.java1.lessons.arrays;
 
 public class Eratosthenes {
 
+    private boolean[] sieve;
+
+    public Eratosthenes(int N){
+
+        sieve = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            sieve[i] = true;
+        }
+        sift(sieve);
+    }
+
     public static int [] eratosthenes (int arrayLength){
         int count=0;
         int [] arrayForCopy = new int[arrayLength];
@@ -73,6 +84,33 @@ public class Eratosthenes {
         }
 
         return arrayForCopy;
+    }
+
+    private static void sift(boolean[] bArray) {
+        for (int a = 2; a < bArray.length-1; a++) {
+            for (int b = 2; b <= a; b++) {
+
+                if ((a%b == 0) & (a/b != 1)) {
+
+                    bArray[a] = false;
+                    System.out.println(a + " - составное число");
+                    System.out.println(bArray[a]);
+                    break;
+                }
+                else if ((a%b==0) & (a/b == 1)) {
+
+                    System.out.println(a + " - простое число");
+                    System.out.println(bArray[a]);
+                    bArray[a] = true;
+                }
+
+
+            }
+        }
+    }
+    public boolean isSimple(int n){
+
+        return sieve[n];
     }
 
     public static void main(String[] args) {
